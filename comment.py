b6aa1062c3937b79bccf9954815b1331b08643b0
🌙 𓋜 𝐀𝐄𝐓𝐇𝐄𝐑 🖤🍷° <lakshmikantagarai123@gmail.com>
2026-06-20 09:10:15 +0530

    Create comment.py

---
+++ comment.py
@@ -0,0 +1,14 @@
+
+#print("Hello World")
+#This is a single line comment.
+print("This is a print statement...")
+print("PY-HANI")#printing PY-HANI
+#These are multiple line comments!
+'''
+Heyy PY-HANI  don't remove thie!
+Author:Kuchupu(Aether!)
+Course:3 months..
+'''
+"""
+Heyy Guyzzz This is PY-HANI,learn Python and chill
+"""
